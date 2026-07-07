8c918e2b9a9a7c2a1df7d15d8924767e66ad789b
Suraj Khillare <skhillare5588@gmail.com>
2026-07-07 11:53:28 +0530

    Initial PinkSnap setup

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on("connection", (socket) => {
+
+    console.log("🟢 User Connected:", socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("🔴 User Disconnected:", socket.id);
+    });
+
+});
+
+server.listen(3000, () => {
+    console.log("🚀 PinkSnap Server Running on http://localhost:3000");
+});
